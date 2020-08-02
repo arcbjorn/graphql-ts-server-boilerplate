fa3b7f36f49f0c0b5d3613d90e7732d7fadd0ab2
Arc <arky.happ@gmail.com>
2020-08-02 18:59:16 +0300

    1.0.0
     - initialized GraphQL Server
     - added resolvers
     - added mutation
     - updated tsconfig
     - added schema.graphql
     - upgraded User entity
     - generated schema declaration
     - added graphql utilities module

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import "reflect-metadata";
+import { GraphQLServer } from 'graphql-yoga'
+
+import { resolvers } from './resolvers'
+
+const server = new GraphQLServer({ typeDefs: 'src/schema.graphql', resolvers })
+server.start(() => console.log('Server is running on localhost:4000'))
